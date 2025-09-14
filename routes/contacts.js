0733205971c69a1c033806d970e59ccdd64522a9
ldsjayhawk <crisbecca@gmail.com>
2025-09-13 23:11:45 -0500

    add put, post & delete routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,4 +5,8 @@ const contactsController = require('../controllers/contacts')
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getIndividual);
 
+router.post('/', contactsController.addIndividual)
+router.put('/:id', contactsController.updateIndividual)
+router.delete('/:id', contactsController.deleteIndividual)
+
 module.exports = router;
